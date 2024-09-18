7bc65a1eb6b63f3b95f16d2ac33f64f387151011
Kevin Will <137236795+N-0-1-R@users.noreply.github.com>
2024-09-18 11:46:35 -0300

    Add files via upload

---
+++ conexão.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "localhost";
+$bancodedados = "aaaaaaaaa";
+$usuario = "root";
+$senha = "";
+
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+if ($mysqli->connect_errno) {
+    echo "Falha ao conectar: (" . $mysqli->connect_errno . ")" . $mysqli->connect_errno;
+
+} else
+    echo "Conectado!";
